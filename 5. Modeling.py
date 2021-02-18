1eff19d6bd94d367b2e0e9d303db9c8dd1083cf5
Tara Crutchfield <taracho98@gmail.com>
2021-02-17 19:12:57 -0800

    editing 5.Modeling for train/test split consistency

--- 5. Modeling.py
+++ 5. Modeling.py
@@ -14,6 +14,23 @@ from keras import layers
 from keras import callbacks
 import gc
 
+def make_y(y,y_test=None,labels=False,gender_split='False'):
+    if gender_split == 'False':
+        y = y.str.split('_',expand=True)[0]
+
+    if labels == False:
+        labels = dict(zip(y.unique(), list(range(len(y.unique())))))
+    y = y.replace(labels)
+    y = keras.utils.to_categorical(y)
+
+    if str(type(y_test)) != "<class 'NoneType'>":
+        y_test = make_y(y=y_test,labels=labels,gender_split=gender_split)
+        y = (y,y_test)
+
+    return y, labels    
+
+#--------------------------------------------------------------------------------------------------------------------
+
 # import legend
 legend = pd.read_csv('Data/CSVs/Audio Legend Clean.csv')[['emotion','filename','sex']].dropna()
 legend['filename'] = legend['filename'].str.replace('.wav','',regex=True)
@@ -27,7 +44,9 @@ for count, file in enumerate(legend['filename'],start=1):
     X.append(input_arr)
     print('%4d of %d complete (%d%%)' % (count,len(legend['filename']),(count/len(legend['filename']))*100),end='\r')
 print('\n')
+
 X = np.array(X)
+y = legend['emotion']+'_'+legend['sex']
 
 # Open JSON file 
 config_name = input("Specify model config json file: ")
@@ -43,24 +62,17 @@ while config_name!= 'quit':
             break_statment = True
         except:
             config_name = input('Something went wrong, please make sure the file exists: ')
-            
-    # set up y variable
-    if config['split_gender'] == 'True':
-        y = legend['emotion']+'_'+legend['sex']
-    if config['split_gender'] == 'False':
-        y = legend['emotion']
 
-    y = pd.factorize(y)
-    
-    labels = list(zip(list(range(len(y[1]))),y[1]))
-    y = y[0]
-    y = keras.utils.to_categorical(y)
-   
-    # split into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=.70,random_state=config['seed'])
-
-    # split test set into a validation and test sets
-    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test,train_size=.33,random_state=config['seed'])
+    if config['final'] == 'False':
+        # split into training and test sets
+        X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=.80,random_state=config['seed'])
+        y,labels = make_y(y_train,y_test,gender_split=config['split_gender'])
+        
+        y_train,y_test = y
+        
+    if config['final'] == 'True':
+        X_train = X
+        y_train,labels = make_y(y,gender_split=config['split_gender'])
 
     # make a model
     model = keras.models.Sequential(name=config['model name'])
@@ -103,35 +115,41 @@ while config_name!= 'quit':
 
     # fit model and print start and end times
     print('\nStart Time:',datetime.now())      
-    history = model.fit(X_train,y_train, validation_data=(X_val, y_val),batch_size=100, 
+    history = model.fit(X_train,y_train, validation_split=0.20,batch_size=100, 
                         epochs=config['epochs'], verbose=1, 
                         callbacks=[MyCustomCallback(),earlystopping])
     print('End Time:  ',datetime.now())
 
     # make a plot of the accuracy with each epoch
-    epoch_accuracy = plt.figure(figsize=(6,4),dpi=100)
-    plot_name = 'Epoch v Accuracy - %s.png' % model.name
-    plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    EVA = plt.figure(figsize=(6,4),dpi=100)
+    plot_name = 'EVA %s' % model.name
+    plt.plot(history.history['accuracy'], label='accuracy',c='#4b3b75')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy',c='#91c64d')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.title(model.name)
+    plt.ylim(0,1)
     plt.legend();
 
     # save plot in Plots
-    epoch_accuracy.savefig('Data/Plots/'+plot_name)
-    predict = model.predict(X_test)
+    EVA.savefig('Data/Plots/'+plot_name)
+    
+    if config['final'] == 'True':
+        keras.model.save('Data/Model/'+str(model.name)+'_final')
+        
+    if config['final'] == 'False':
+        predict = model.predict(X_test)
 
-    predictions = pd.DataFrame(predict).rename(columns=dict(labels))
-    predictions['predict_int'] = [np.argmax(entry) for entry in predict]
-    predictions['true_int'] = [np.argmax(entry) for entry in y_test]
-    predictions['true_emo'] = predictions['true_int'].replace(dict(labels))
+        predictions = pd.DataFrame(predict).rename(columns=dict(labels))
+        predictions['predict_int'] = [np.argmax(entry) for entry in predict]
+        predictions['true_int'] = [np.argmax(entry) for entry in y_test]
+        predictions['true_emo'] = predictions['true_int'].replace(dict(labels))
 
-    accuracy = int(len(predictions[predictions['predict_int'] == predictions['true_int']])/len(predictions)*100)
-    print('\nModel:',model.name)
-    print('Accuracy: %d%%' % accuracy)
+        accuracy = int(len(predictions[predictions['predict_int'] == predictions['true_int']])/len(predictions)*100)
+        print('\nModel:',model.name)
+        print('Accuracy: %d%%' % accuracy)
 
-    path = 'Data/CSVs/Predictions_%s.csv' % (model.name)
-    predictions.to_csv(path)
+        path = 'Data/CSVs/Predictions_%s.csv' % (model.name)
+        predictions.to_csv(path)
 
     config_name = input("\nIf you wish to end the program please type 'quit'.\nIf you wish to test another model, please enter config json: ")
